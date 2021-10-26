eb2cbae3deedd84d579aae62dcd13287539552e1
k-737 <kamronalimov80@gmail.com>
2021-10-26 22:00:07 +0500

    body created

--- src/components/MainBody/Body/index.jsx
+++ src/components/MainBody/Body/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Container, Title } from "./style";
+import { Button, Container, Title } from "./style";
 import title from "../../../assets/img/title.png";
+import title2 from "../../../assets/img/title2.png";
 
 const Body = () => {
   return (
     <Container>
-      <Title src={title} />
+      <Title main src={title} />
+      <Title top src={title2} />
+      <Button>Расчет стоимости</Button>
     </Container>
   );
 };
--- src/components/MainBody/Body/style.js
+++ src/components/MainBody/Body/style.js
@@ -3,6 +3,7 @@ import Bg from "../../../assets/img/bg.png";
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 1000px;
@@ -10,6 +11,22 @@ export const Container = styled.div`
   background-size: cover;
 `;
 export const Title = styled.img`
-  width: 748px;
-  height: 124px;
+  width: ${({ main }) => (main ? `748px` : `410px`)};
+  height: ${({ main }) => (main ? `124px` : `74px`)};
+  margin-top: ${({ top }) => top && `30px`};
+`;
+export const Button = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 50px;
+  width: 220px;
+  height: 62px;
+  background: #ffffff;
+  border-radius: 50px;
+  cursor: pointer;
+  transition: all 0.3s;
+  :hover {
+    background: #e1dece;
+  }
 `;
