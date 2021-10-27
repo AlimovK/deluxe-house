51799ed6331453b559cb444e8aad9c6671b58223
k-737 <kamronalimov80@gmail.com>
2021-10-27 20:06:33 +0500

    body changed

--- src/components/MainBody/Body/index.jsx
+++ src/components/MainBody/Body/index.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Button, Container, Title } from "./style";
 import title from "../../../assets/img/title.png";
 import title2 from "../../../assets/img/title2.png";
+import Zoom from "react-reveal/Zoom";
 
 const Body = () => {
   return (
     <Container>
-      <Title main src={title} />
-      <Title top src={title2} />
+      <Zoom right cascade>
+        <Title main src={title} />
+        <Title top src={title2} />
+      </Zoom>
       <Button>Расчет стоимости</Button>
     </Container>
   );
